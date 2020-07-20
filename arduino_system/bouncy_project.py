c04e4f97022eb0c5a8c83e5e18bd61277e14d297
Edfame <edfame@gmail.com>
2020-07-20 19:51:22 +0100

    Reading data from the serial correctly.

--- arduino_system/bouncy_project.py
+++ arduino_system/bouncy_project.py
@@ -1,18 +1,28 @@
+import yaml
 import serial
-import time
-import csv
 
-ser = serial.Serial('COM3', 9600)
+arduino_port = None
+arduino_rate = None
+
+with open(r'cfg/arduino_cfg.yaml') as yaml_file:
+
+	arduino = yaml.load(yaml_file, Loader=yaml.FullLoader).get("arduino")
+
+	arduino_port = arduino.get("port")
+	arduino_rate = arduino.get("rate")
+
+
+ser = serial.Serial(arduino_port, arduino_rate)
 ser.flushInput()
 
 while True:
     try:
         ser_bytes = ser.readline()
-        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+        decoded_bytes = str(ser_bytes[0:len(ser_bytes)-1].decode("utf-8"))
         print(decoded_bytes)
-        with open("test_data.csv","a") as f:
-            writer = csv.writer(f,delimiter=",")
-            writer.writerow([time.time(),decoded_bytes])
+
+        #TODO write to the database.
+        
     except:
         print("Keyboard Interrupt")
         break
