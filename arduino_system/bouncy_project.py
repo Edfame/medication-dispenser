0ddb4c689be0328cab7ef9f912951c06fcd05df6
Edfame <edfame@gmail.com>
2020-07-20 19:59:45 +0100

    Added date ant time.

--- arduino_system/bouncy_project.py
+++ arduino_system/bouncy_project.py
@@ -1,5 +1,6 @@
 import yaml
 import serial
+from datetime import datetime 
 
 arduino_port = None
 arduino_rate = None
@@ -16,13 +17,16 @@ ser = serial.Serial(arduino_port, arduino_rate)
 ser.flushInput()
 
 while True:
-    try:
-        ser_bytes = ser.readline()
-        decoded_bytes = str(ser_bytes[0:len(ser_bytes)-1].decode("utf-8"))
-        print(decoded_bytes)
 
-        #TODO write to the database.
-        
-    except:
-        print("Keyboard Interrupt")
-        break
+	instant = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+
+	try:
+	    ser_bytes = ser.readline()
+	    decoded_bytes = str(ser_bytes[0:len(ser_bytes)-1].decode("utf-8"))
+	    print(instant + " > " + decoded_bytes)
+
+	    #TODO write to the database.
+	    
+	except:
+	    print("Keyboard Interrupt")
+	    break
