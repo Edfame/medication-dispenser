1b411bad90f008864761c6c776b2fc3c7efebb47
Edfame <edfame@gmail.com>
2020-08-21 16:08:32 +0100

    Fully functional Python APP. Listens from arduino and then posts to the API.

--- arduino_system/bouncy_project.py
+++ arduino_system/bouncy_project.py
@@ -6,6 +6,15 @@ from datetime import datetime
 #Constants
 closed_hatch = "CLOSED HATCH"
 
+class Controller:
+
+	def __init__(self, cereal, configs, identifications):
+		self.cereal = cereal
+		self.cfgs = configs
+		self.api = configs.get("api")
+		self.user = identifications.get("user")
+		self.drugs = identifications.get("drugs")
+		
 
 # Load the configs from the file provided.
 def load_configs():
@@ -16,47 +25,63 @@ def load_configs():
     return configs
 
 
-# Connect to the serial port provided.
-def connect_to_serial(port, rate):
+# Load user and drugs.
+def load_identications():
 
-    ser = serial.Serial(port, rate)
+	with open(r'identifications.yaml') as yaml_file:
+		identifications = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+	return identifications
+
+
+# Connect to the serial port provided.
+def connect_to_serial(cfgs):
+
+    ser = serial.Serial(cfgs.get("port"), cfgs.get("rate"))
     ser.flushInput()
 
     return ser
 
 
-def write_new_data(message, api):
+# Posts the new administration data to the API.
+def write_new_data(timestamp, controller):
 
     obj ={
-        "drugName": message
+        "administrationId":{
+        	"drugId": controller.drugs[0],
+        	"userId": controller.user.get("id")
+        },
+        "administrationTimestamp": timestamp
     }
 
-    request = requests.post(api, json=obj)
+    request = requests.post(controller.api, json=obj)
     print(request.json())
 
 
 # Start the arduino listener.
-def run(ser, api):
+def run(controller):
     while True:
 
-        instant = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+        timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
         try:
-            ser_bytes = ser.readline()
+            ser_bytes = controller.cereal.readline()
             decoded_bytes = str(ser_bytes[0:len(ser_bytes) - 1].decode("utf-8"))
-            print(instant + " > " + decoded_bytes)
+            print(timestamp + " > " + decoded_bytes)
 
             if decoded_bytes == closed_hatch:
-                write_new_data(instant, api)
+                write_new_data(timestamp, controller)
 
-        except:
-            print("Keyboard Interrupt")
-            break
+        except Exception as e:
+        	print(e)
 
 
 if __name__ == "__main__":
 
     # puns intended
     cfgs = load_configs()
-    cereal = connect_to_serial(cfgs.get("port"), cfgs.get("rate"))
-    run(cereal, cfgs.get("api"))
+    identifications = load_identications()
+    cereal = connect_to_serial(cfgs)
+    ctrl = Controller(cereal, cfgs, identifications)
+
+    run(ctrl)
